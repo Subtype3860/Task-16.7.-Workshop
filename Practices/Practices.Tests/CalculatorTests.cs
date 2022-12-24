db867cd433cbabf614626532f2a41120abe95b85
Subtype3860 <booravoi@gmail.com>
2022-12-24 18:31:59 +0300

    Finish project

---
+++ Practices/Practices.Tests/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            var result = calc.Additional(20, 80);
+            Assert.AreEqual(100, result);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            var result = calc.Subtraction(90, 90);
+            Assert.Zero(result);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            var result = calc.Miltiplication(2, 5);
+            Assert.True(10 == result);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            var result = calc.Division(4, 2);
+            Assert.AreNotEqual(1, result);
+        }
+    }
+}
